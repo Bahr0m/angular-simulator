0f0c6ad967209a88e5d376d1ac19f9cb39384ce3
Bahrom <sattorovbahrom20@gmail.com>
2026-05-24 00:44:07 +0500

    Revert "finish header"
    This reverts commit 4c54627329bfd72872f97ab7948829ba2bb60e20.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,10 @@
 import { Component } from '@angular/core';
-import { Colors } from '../enums/Color';
-import './collection';
-import { SelectComponent } from './common_ui/select/select.component';
+import "./training"
 @Component({
   selector: 'app-root',
-  imports: [SelectComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  company_name = 'РУМТИБЕТ';
-  selects: Omit<SelectComponent, 'isOpen' | 'toggleDropdown'>[] = [
-    {
-      options: [
-        { label: 'Париж', value: 'paris' },
-        { label: 'Токио', value: 'tokyo' },
-        { label: 'Нью-Йорк', value: 'newyork' },
-        { label: 'Барселона', value: 'barcelona' },
-        { label: 'Амстердам', value: 'amsterdam' },
-        { label: 'Рим', value: 'rome' },
-      ],
-      placeholder: 'Локация для тура',
-      desc: 'Выберите из списка',
-      type: 'select',
-    },
-    {
-      options: [
-        { label: 'Январь', value: '1' },
-        { label: 'Февраль', value: '2' },
-        { label: 'Март', value: '3' },
-        { label: 'Апрель', value: '4' },
-        { label: 'Май', value: '5' },
-        { label: 'Июнь', value: '6' },
-      ],
-      placeholder: 'Дата похода',
-      desc: 'укажите дату',
-      type: 'date',
-    },
-    {
-      options: [
-        { label: 'Максим', value: '1' },
-        { label: 'Анна', value: '2' },
-        { label: 'Федор', value: '3' },
-        { label: 'Алина', value: '4' },
-        { label: 'Виктор', value: '5' },
-        { label: 'Светлана', value: '6' },
-      ],
-      placeholder: 'Участники',
-      desc: 'выберите из списка',
-      type: 'select',
-    },
-  ];
-
-  //2
-  isMainColor(color: string): boolean {
-    return color === Colors.Red || color === Colors.Green || color === Colors.Blue;
-  }
-
-  //3
-  constructor() {
-    let now: string = new Date().toLocaleDateString();
-    localStorage.setItem('lastDate', now);
-
-    //4
-    let time: number = +(localStorage.getItem('time') || 0);
-    localStorage.setItem('time', (++time).toString());
-  }
 }
