6f12d649d55175c5e945b2444824218b9318d536
Bahrom <sattorovbahrom20@gmail.com>
2026-05-24 00:55:08 +0500

    finish hw_15

---
+++ src/enums/Color.ts
@@ -0,0 +1,12 @@
+export enum Colors {
+    Red = "Red",
+    Green = "Green",
+    Blue = "Blue",
+    Yellow = "Yellow",
+    Orange = "Orange",
+    Purple = "Purple",
+    Pink = "Pink",
+    Brown = "Brown",
+    Black = "Black",
+    White = "White"
+}
