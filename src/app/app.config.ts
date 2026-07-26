95b83b3d5f81597e2234a2923f1b8ba7e6790099
Bahrom <sattorovbahrom20@gmail.com>
2026-07-26 17:26:25 +0500

    themes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,7 +15,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection(),
     providePrimeNG({
       theme: {
-        // preset: Aura,
+        preset: Aura,
+        options: {
+            darkModeSelector: '.dark_mode'
+        }
       },
     }),
   ],
--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -1,6 +1,22 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs';
+import { LocalStoreService } from '../localStor/local-store.service';
 @Service()
 export class ThemeService {
-  
+  localStorageService: LocalStoreService = inject(LocalStoreService);
+  private localStorageKey = 'theme';
+  private themeSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
+    this.localStorageService.get(this.localStorageKey) || 'Aura',
+  );
+  theme$ = this.themeSubject.asObservable();
+
+  setTheme(theme: string): void {
+    this.localStorageService.set(this.localStorageKey, theme);
+    this.themeSubject.next(theme);
+  }
+
+  getTheme(): string {
+    return this.themeSubject.value;
+  }
 }
