53a76236aa435a5301607f1be1c40b31c0c6a78e
Bahrom <sattorovbahrom20@gmail.com>
2026-08-15 18:23:27 +0500

    fix phone-pipe

--- src/app/helpers/pipes/phone.pipe.ts
+++ src/app/helpers/pipes/phone.pipe.ts
@@ -1,26 +1,39 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-
-
 @Pipe({
   name: 'phone',
   standalone: true,
 })
 export class PhonePipe implements PipeTransform {
-
   transform(value: string, type: string): string {
     const phone_number = value.replace(/\D/g, '');
-    if(type === 'compact') {
-      return "+" + phone_number;
+
+    if (phone_number.length !== 12) {
+      return String(value);
     }
-    if(type === 'international') {
-      return "+" + phone_number.slice(0, 1) + " " + phone_number.slice(1, 4) + " " + phone_number.slice(4, 7) + " " + phone_number.slice(7, 9) + " " + phone_number.slice(9, 11);
+    
+    if (type === 'compact') {
+      return '+' + phone_number;
     }
-    if(type === 'national') {
+    if (type === 'international') {
+      return (
+        '+' +
+        phone_number.slice(0, 1) +
+        ' ' +
+        phone_number.slice(1, 4) +
+        ' ' +
+        phone_number.slice(4, 7) +
+        ' ' +
+        phone_number.slice(7, 9) +
+        ' ' +
+        phone_number.slice(9, 11)
+      );
+    }
+    if (type === 'national') {
       return phone_number.slice(4, 7) + phone_number.slice(7, 9) + phone_number.slice(9, 11);
     }
-    if(type === 'masked') {
-      return phone_number.slice(0, 4) + "****" + phone_number.slice(8, 11);
+    if (type === 'masked') {
+      return phone_number.slice(0, 4) + '****' + phone_number.slice(8, 11);
     }
     return value;
   }
