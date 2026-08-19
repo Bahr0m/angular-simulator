69800421220a481b3b1a3b2226bb323f195817f4
Bahrom <sattorovbahrom20@gmail.com>
2026-08-20 00:30:06 +0500

    fix catch_error

--- src/app/helpers/interceptors/catch-error.interceptor.ts
+++ src/app/helpers/interceptors/catch-error.interceptor.ts
@@ -4,15 +4,15 @@ import { catchError, throwError } from 'rxjs';
 import { MessageService } from '../../services/message/message.service';
 
 export const catchErrorInterceptor: HttpInterceptorFn = (req, next) => {
-  const messages = inject(MessageService);
+  const messageService = inject(MessageService);
 
   return next(req).pipe(
     catchError((error) => {
       if (error.status >= 500) {
         console.error('HTTP Error:', error);
-        messages.showError('Произошла ошибка на сервере. Пожалуйста, попробуйте позже.');
+        messageService.showError(`Ошибка сервера (${error.status}). Попробуйте позже.`);
       }
       return throwError(() => error);
-    })
+    }),
   );
 };
